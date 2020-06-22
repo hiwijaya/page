59f028f8e98c4da78e656fc62907fc9a28a3752b
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-06-22 17:36:05 +0700

    styling landing page

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import Brand from '../components/Brand';
 import Button from '../components/Button';
+import illustration from '../images/illustration-1.png';
 
 export default class Landing extends Component {
 
@@ -13,6 +14,23 @@ export default class Landing extends Component {
                         <a href="/about">About</a>
                         <a href="/signin">Sign In</a>
                         <Button title={'Register'} />
+                    </div>
+                </div>
+                <div className="landing-content">
+                    <div className="column center-vertically">
+                        <h1>
+                            Because <br/>
+                            bookmark bar in <br/>
+                            your browser is <br/>
+                            not enough.
+                        </h1>
+                        <Button title={'Get Started'}/>
+                    </div>
+                    <div className="column illustration">
+                        <img src={illustration} alt="Illustration"/>
+                        <span className="attribution">
+                            Art by <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">Ouch.pics</a>
+                        </span>
                     </div>
                 </div>
             </div>
