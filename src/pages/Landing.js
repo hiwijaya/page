798c8aa6613465d9ad53a145f2b17c1b3928014d
Happy Indra Wijaya <dev.hiwijaya@gmail.com>
2020-07-01 17:03:23 +0700

    migrate to Hook

--- src/pages/Landing.js
+++ src/pages/Landing.js
@@ -1,46 +1,42 @@
 import React, { Component } from 'react';
+import { useHistory } from 'react-router-dom';
 import Brand from '../components/Brand';
 import Button from '../components/Button';
 import illustration from '../images/illustration-1.png';
 
-export default class Landing extends Component {
+const Landing = _ => {
 
-    navigateTo(path){
+    const history = useHistory();
 
-        this.props.history.push(path);
-
-    }
-
-    render() {
-        return (
-            <div className="page">
-                <div className="landing-header">
-                    <Brand />
-                    <div className="landing-menu">
-                        <a href="/about">About</a>
-                        <a href="/signin">Sign In</a>
-                        <Button title={'Register'} onClick={() => this.navigateTo('/signup')}/>
-                    </div>
-                </div>
-                <div className="landing-content">
-                    <div className="column center-vertically">
-                        <h1>
-                            Because <br/>
-                            bookmark bar in <br/>
-                            your browser is <br/>
-                            not enough.
-                        </h1>
-                        <Button title={'Get Started'} onClick={() => this.navigateTo('/signup')}/>
-                    </div>
-                    <div className="column illustration">
-                        <img src={illustration} alt="Illustration"/>
-                        <span className="attribution">
-                            Art by <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">Ouch.pics</a>
-                        </span>
-                    </div>
+    return(
+        <div className="page">
+            <div className="landing-header">
+                <Brand />
+                <div className="landing-menu">
+                    <a href="/about">About</a>
+                    <a href="/signin">Sign In</a>
+                    <Button title={'Register'} onClick={() => history.push('/signup')} />
                 </div>
             </div>
-        );
-    }
-    
+            <div className="landing-content">
+                <div className="column center-vertically">
+                    <h1>
+                        Because <br />
+                            bookmark bar in <br />
+                            your browser is <br />
+                            not enough.
+                        </h1>
+                    <Button title={'Get Started'} onClick={() => history.push('/signup')} />
+                </div>
+                <div className="column illustration">
+                    <img src={illustration} alt="Illustration" />
+                    <span className="attribution">
+                        Art by <a href="https://icons8.com" target="_blank" rel="noopener noreferrer">Ouch.pics</a>
+                    </span>
+                </div>
+            </div>
+        </div>
+    );
+
 }
+export default Landing;
